1cde9bfd70803c94e3e770383e67ee2512740deb
raj thakur <rajthakur913124@gmail.com>
2025-06-01 22:44:28 +0530

    commited change

--- Aurbientech/settings.py
+++ Aurbientech/settings.py
@@ -114,13 +114,13 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
-import os
-# Ensure these settings are correct:
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # For production
+
+STATIC_URL = 'static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),  # Your local static files
+    BASE_DIR / "static",  # This is where your static files will be stored
 ]
+STATIC_ROOT = BASE_DIR / "staticfiles"  # For collecting static files during deployment
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
