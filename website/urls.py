6ddde0a4fbf0d8e1e5e12c9fbf33371138363d11
raj thakur <rajthakur913124@gmail.com>
2025-06-01 22:11:18 +0530

    commited change

--- website/urls.py
+++ website/urls.py
@@ -6,4 +6,8 @@ app_name = 'website'
 urlpatterns = [
     path('', views.home, name='home'),
     path('services/', views.services, name='services'),
+    path('about/', views.about, name='about'),
+    path('contact/', views.contact, name='contact'),
+    path("blog/",views.blogs,name='blog'),
+    path("casestudies/",views.casestudy,name='casestudies'),
 ]
--- website/views.py
+++ website/views.py
@@ -5,3 +5,13 @@ def home(request):
     return render(request, 'website/home.html')
 def services(request):
     return render(request,'website/services.html')
+def about(request):
+    return render(request,'website/about.html')
+def contact(request):
+    return render(request,'website/contact.html')
+def blogs(request):
+    return render(request,"website/blogs.html")
+def casestudy(request):
+    return render(request,'website/case-studies.html')
+def portfolio(request):
+    return render(request,'website/pop.html')
